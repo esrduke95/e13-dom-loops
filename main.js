81c0b074a2c5476696ac11b0c85744dcf33ecb02
Summer Duke <esrduke@gmail.com>
2020-07-21 19:07:21 -0500

    basics

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-console.log('gonna loop on the dom so hard man');
+// const ageCheck = (age) => {
+//     if (age < 21) {
+//         console.log("No drinks for you!");
+//     } else {
+//         console.log("PARTAY!!!");
+//     }
+// }
+
+// ageCheck(12);
+// ageCheck(21);
+
+// const lengthOutput = (string) => {
+//     return string.length
+// }
+
+// console.log(lengthOutput('teresa'));
+
+// const quoteFinder = (testString) => {
+//     return "winter is coming".indexOf(testString);
+// }
+
+// console.log(quoteFinder('is'));
+
+const employee = {
+    firstName: 'Summer',
+    lastName: 'Duke',
+    role: 'babyCallRep',
+    accountNumber: '666',
+    isManager: false,
+    titles: ['Call Center Rep', 'jackass'],
+    accolades: {
+        title: 'none designer with left developer',
+        expires: 'soon i hope',
+        isCool: 'false'
+    }
+
+}
+// console.log(employee.FirstName);
+// const keyToCheck = 'accolade';
+
+// console.log()
+
+// const employeeFunction = (employee) => {
+//     if (employee.firstName === 'S') {
+//         Object.status = "vip"
+//     } else {
+//         Object.status = "peasant"
+//     }
+// }
+
+// console.log(employeeFunction(employee));
+
+//fix later
